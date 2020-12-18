4df10669d1ea720a17d32588a74131a7c0eb61a5
Ayush <ayushkhandelwal3217@gmail.com>
2020-12-18 16:14:59 +0530

    app deployed

--- src/components/HeaderComponents/HeaderNav.jsx
+++ src/components/HeaderComponents/HeaderNav.jsx
@@ -1,8 +1,6 @@
 import Axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import ReactCountryFlag from "react-country-flag"
-import { Button, Menu } from "@material-ui/core";
-import MenuItem from '@material-ui/core/MenuItem';
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket"
 import { Link } from 'react-router-dom';
 import { useBasket } from '../../contexts/Basket';
@@ -13,17 +11,7 @@ function HeaderNav() {
     // eslint-disable-next-line
     const [state, dispatch] = useBasket();
 
-    const [anchorEl, setAnchorEl] = useState(null);
-
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-
-    let user=state.user;
+    let user = state.user;
 
     const [locInfo, setLocInfo] = useState({ status: "success", country: "India", countryCode: "IN", region: "DL" })
 
@@ -44,34 +32,22 @@ function HeaderNav() {
                 <span className="nav_links_line1"></span>
                 <span className="nav_links_line2"><ReactCountryFlag countryCode={locInfo.countryCode} /></span>
             </div>
-            {user ? (
-                <div>
-                    <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+            <Link to="/signin">
+                {
+                    user ? (
                         <div className="header_nav_links">
                             <span className="nav_links_line1">Hello {user.displayName}</span>
-                            <span className="nav_links_line2">Sign In</span>
+                            <span className="nav_links_line2" onClick={() => auth.signOut()} >Sign Out</span>
                         </div>
-                    </Button>
-                    <Menu
-                        id="simple-menu"
-                        anchorEl={anchorEl}
-                        keepMounted
-                        open={Boolean(anchorEl)}
-                        onClose={handleClose}
-                    >
-                        <MenuItem onClick={handleClose}>Profile</MenuItem>
-                        <MenuItem onClick={() => auth.signOut()}>Logout</MenuItem>
-                    </Menu>
-                </div>
-            ) : (
-                    <Link to="/signin">
-                        <div className="header_nav_links">
-                            <span className="nav_links_line1">Hello Guest</span>
-                            <span className="nav_links_line2">Sign In</span>
-                        </div>
-                    </Link>
-                )}
+                    ) : (
+                            <div className="header_nav_links">
+                                <span className="nav_links_line1">Hello Guest</span>
+                                <span className="nav_links_line2">Sign In</span>
+                            </div>
+                        )
+                }
 
+            </Link>
             <div className="header_nav_links">
                 <span className="nav_links_line1">Returns</span>
                 <span className="nav_links_line2">& orders</span>
